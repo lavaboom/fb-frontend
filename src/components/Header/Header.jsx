342bff43330f2a0594f7661e22ba69c1762f113d
Smoothie <lavaboom@yahoo.com>
2022-01-24 21:49:26 -0500

    refactored CSS style in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,27 +16,27 @@ export default class Header extends Component {
 
     render() {
         return (
-            <header class="header">
-                <nav class="navbar">
-                    <a href="#" class="nav-logo">WebDev.</a>
-                    <ul class={`nav-menu ${ this.state.mobileMenu ? 'active' : '' }`}> 
-                        <li class="nav-item">
-                        <a href='#' class='nav-link' onClick={ this.toggleMobileMenu }>Services</a>
+            <header class='header'>
+                <nav class='navbar'>
+                    <a href='#' class='navbar__logo'>Food Bunnies</a>
+                    <ul class={`navbar__menu ${ this.state.mobileMenu ? 'navbar__menu--active' : '' }`}> 
+                        <li class='navbar__item'>
+                        <a href='#' class='navbar__link' onClick={ this.toggleMobileMenu }>Services</a>
                         </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link" onClick={ this.toggleMobileMenu }>Blog</a>
+                        <li class='navbar__item'>
+                            <a href='#' class='navbar__link' onClick={ this.toggleMobileMenu }>Blog</a>
                         </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link" onClick={ this.toggleMobileMenu }>About</a>
+                        <li class='navbar__item'>
+                            <a href='#' class='navbar__link' onClick={ this.toggleMobileMenu }>About</a>
                         </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link" onClick={ this.toggleMobileMenu }>Contact</a>
+                        <li class='navbar__item'>
+                            <a href='#' class='navbar__link' onClick={ this.toggleMobileMenu }>Contact</a>
                         </li>
                     </ul>
-                    <div class={`hamburger ${ this.state.mobileMenu ? 'active' : '' }`} onClick={ this.toggleMobileMenu }> 
-                        <span class="bar"></span>
-                        <span class="bar"></span>
-                        <span class="bar"></span>
+                    <div class={`hamburger ${ this.state.mobileMenu ? 'hamburger--active' : '' }`} onClick={ this.toggleMobileMenu }> 
+                        <span class='hamburger__bar'></span>
+                        <span class='hamburger__bar'></span>
+                        <span class='hamburger__bar'></span>
                     </div>
                 </nav>
             </header>
