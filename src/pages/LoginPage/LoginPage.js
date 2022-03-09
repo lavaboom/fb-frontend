29feaf41869ece71017c0fd28dd7a71584ddb52c
Smoothie <lavaboom@yahoo.com>
2022-03-09 11:47:05 -0500

    added hooks to login page

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,58 +1,53 @@
 // React modules
-import React, { Component } from 'react'
+import React from 'react';
 import { Redirect, Link } from 'react-router-dom';
 // app styles & assets
 import './LoginPage.scss'
 // other sub components
 import Input from '../../components/Input/Input';
 // redux
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { login } from '../../store/authReducer';
 
-class LoginPage extends Component {
+const LoginPage = () => {
 
-    handleSubmit = (event) => {
+    // returns the dispatch function of the redux store
+    const dispatch = useDispatch();
+
+    // subscribe to slices of the store we're interested in
+    let isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+    let error = useSelector(state => state.auth.error);
+
+    // event handlers
+    let handleSubmit = (event) => {
         event.preventDefault();
         const data = {
             email: event.target.email.value,
             password: event.target.password.value
         }
-        this.props.login(data);
+        dispatch(login(data));
     }
 
-    render() {
-        return (
-            <main className='login-page'>
-                <form className='login' onSubmit={ this.handleSubmit }>
-                    <h1 className='login__title'>Log in</h1>
+    return (
+        <main className='login-page'>
+            <form className='login' onSubmit={ handleSubmit }>
+                <h1 className='login__title'>Log in</h1>
 
-                    <Input type='text' name='email' label='Email' />
-                    <Input type='password' name='password' label='Password' />
-                
-                    <button className='login__button'>Log in</button>
+                <Input type='text' name='email' label='Email' />
+                <Input type='password' name='password' label='Password' />
+            
+                <button className='login__button'>Log in</button>
 
-                    {this.props.error && <div className='login__message'>{this.props.error}</div>}
-                    {this.props.isLoggedIn && <Redirect to='/kitchen' />}
+                {error && <div className='login__message'>{error}</div>}
+                {isLoggedIn && <Redirect to='/kitchen' />}
 
-                    {/* {this.state.userType === 'Kitchen' ? <Redirect to='/kitchen' /> : <Redirect to='/driver' />} */}
-                </form>
-                <p>
-                    Need an account? <Link to='/signup'>Sign up</Link>
-                </p>
-            </main>
-        );
-    }
+                {/* {this.state.userType === 'Kitchen' ? <Redirect to='/kitchen' /> : <Redirect to='/driver' />} */}
+            </form>
+            <p>
+                Need an account? <Link to='/signup'>Sign up</Link>
+            </p>
+        </main>
+    );
 }
-
-// subscribe to the auth slice of the store
-const mapStateToProps = state => ({
-    isLoggedIn: state.auth.isLoggedIn,
-    error: state.auth.error
-})
-
-// dispatch function to modify the store
-const mapDispatchToProps = dispatch => ({
-    login: (data) => dispatch(login(data)),
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(LoginPage)
+ 
+export default LoginPage;
